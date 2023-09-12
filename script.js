52e9e05100706d9cde8821e7373a59c17d3770ef
mironshoh08 <salimovmrionshoh09@gmail.com>
2023-09-12 13:39:02 +0500

    main

---
+++ script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    swal("Help", "Tel +998 95 078 27 09 \n E-mail : salimovmironshoh07@gmail.com");
+}
+/* function myFunction1() {
+    swal({
+        title: "Sign In",
+        text: 'Create Your Accaunt 👇', 
+        content: "input" ,
+        button: {
+            text: "Search!",
+            closeModal: false,
+        },
+    })
+        .then((value) => {
+            swal({
+                title: "Good job!",
+                text: "You clicked the button!",
+                icon: "success",
+                button: "Aww yiss!",
+            });
+        });
+}
+function myFunction2() {
+    swal("Help", "Tel +998 95 078 27 09 \n E-mail : salimovmironshoh07@gmail.com");
+}   */
